ed64f81945faa57c1c6c744f78d08500853c2a8d
Gabriel Gilberto <gabrielgilberto@fisica.ufc.br>
2022-02-09 16:43:41 -0300

    First commit - 16:43 Brazil - 02/09/2022

---
+++ exercicio01.js
@@ -0,0 +1,36 @@
+// Tipo Number
+
+const meuNumero = 1;
+
+const primeiroNumero = 2;
+const segundoNumero = 3;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+const novaOperacaoMatematica = primeiroNumero * segundoNumero
+
+console.log(novaOperacaoMatematica);
+
+// Tipo string
+
+const texto1 = "'Olá, mundo!'"
+const texto2 = "'Hello, World!'"
+
+const novoTexto = texto1 + " é o mesmo que " + texto2
+
+console.log(novoTexto);
+
+// Tipo boolean
+
+const terceiroNumero = 5
+const quartoNumero = 5
+let cadastroAtivado = true
+
+console.log(terceiroNumero === quartoNumero);
+
+const texto3 = "Alura"
+const texto4 = "alura"
+
+console.log(texto3 === texto4);  
